544227aab3133ad777bff4b16c426c7eea2bea27
DESKTOP-JU2NB2K\rlaal <rlaalsxo1990@gmail.com>
2022-09-22 22:44:37 +0900

    게이트웨이 필터적용

---
+++ src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,53 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
+
+            ServerHttpRequest serverHttpRequest = exchange.getRequest();
+            ServerHttpResponse serverHttpResponse = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage : {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Logging PRE Filter : request id -> {}", serverHttpRequest.getId());
+            }
+            // Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPreLogger()) {
+                    log.info("Logging POST Filter : response code -> {}", serverHttpResponse.getStatusCode());
+                }
+            }));
+        }, Ordered.LOWEST_PRECEDENCE);
+
+        return filter;
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+}
